5ae0a98d9db849eaa6867e32fe6b2eb535a7e882
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-19 14:23:28 +0200

    feat: create an empty vue component to hold app lifecycles

---
+++ src/components/SolarSystem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="solar-system" />
+</template>
+
+<style scoped>
+#solar-system {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
