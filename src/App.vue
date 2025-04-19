3161aa4c9d9277f5f1b4aef888cef622c1e95064
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-19 14:24:50 +0200

    feat: linking the SolarSystem.vue component to App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import SolarSystem from '@/components/SolarSystem.vue'
 </script>
 
 <template>
-  <div />
+  <div class="app-container">
+    <SolarSystem />
+  </div>
 </template>
 
-<style scoped>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.app-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 </style>
