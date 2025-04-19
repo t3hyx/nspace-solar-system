18d24d7aa0c8863a8201d6bf96748640ad7202cf
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-19 13:58:07 +0200

    fix: adding ability for Vite to resolve paths alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+const root = path.resolve(__dirname, 'src')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': root,
+    },
+  },
 })
